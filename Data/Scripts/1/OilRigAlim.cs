85bb8d73790f0d72794f729671e4e7924685797a
CaptainHarlock0006 <94286908+CaptainHarlock0006@users.noreply.github.com>
2022-01-08 17:29:04 +0100

    Script anpassungen

--- Data/Scripts/1/OilRigAlim.cs
+++ Data/Scripts/1/OilRigAlim.cs
@@ -133,21 +133,111 @@ namespace P3DResourceRig
             }
         }
 
-        private void DrawBeams()
-        {
-            var maincolor = PrimaryBeamColor.ToVector4();
-            var auxcolor = SecondaryBeamColor.ToVector4();
-
-            VRage.Game.MySimpleObjectDraw.DrawLine(m_generator.WorldAABB.Center - (m_generator.WorldMatrix.Down * 2.5), m_generator.WorldAABB.Center + (m_generator.WorldMatrix.Down * 2.5 * 4), VRage.Utils.MyStringId.GetOrCompute("WeaponLaser"), ref auxcolor, 0.33f);
-            VRage.Game.MySimpleObjectDraw.DrawLine(m_generator.WorldAABB.Center - (m_generator.WorldMatrix.Down * 2.5), m_generator.WorldAABB.Center + (m_generator.WorldMatrix.Down * 2.5 * 4), VRage.Utils.MyStringId.GetOrCompute("WeaponLaser"), ref maincolor, 1.02f);
-
-            // Draw 'pulsing' beam
-            if (m_counter % 2 == 0)
-            {
-                VRage.Game.MySimpleObjectDraw.DrawLine(m_generator.WorldAABB.Center - (m_generator.WorldMatrix.Down * 2.5), m_generator.WorldAABB.Center + (m_generator.WorldMatrix.Down * 2.5 * 4), VRage.Utils.MyStringId.GetOrCompute("WeaponLaser"), ref maincolor, 1.12f);
-            }        
-        }
     }
+     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Assembler), true, "LargeBlockRig2")]
+    public class Rig3 : MyGameLogicComponent
+    {
+        // Builder is nessassary for GetObjectBuilder method as far as I know.
+        private MyObjectBuilder_EntityBase builder;
+        private Sandbox.ModAPI.IMyAssembler m_generator;
+        private IMyCubeBlock m_parent;
+        #region IsInVoxel definition
+        private bool IsInVoxel(Sandbox.ModAPI.IMyTerminalBlock block)
+        {
+            BoundingBoxD blockWorldAABB = block.PositionComp.WorldAABB;
+            List<MyVoxelBase> voxelList = new List<MyVoxelBase>();
+            MyGamePruningStructure.GetAllVoxelMapsInBox(ref blockWorldAABB, voxelList);
+            var cubeSize = block.CubeGrid.GridSize;
+            BoundingBoxD localAAABB = new BoundingBoxD(cubeSize * ((Vector3D)block.Min - 1), cubeSize * ((Vector3D)block.Max + 1));
+            var gridWorldMatrix = block.CubeGrid.WorldMatrix;
+            foreach (var map in voxelList)
+            {
+                if (map.IsAnyAabbCornerInside(ref gridWorldMatrix, localAAABB))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        #endregion
+        #region colors
+        private Color m_primaryColor = Color.OrangeRed;
+        private Color m_secondaryColor = Color.LemonChiffon;
+        public Color PrimaryBeamColor
+        {
+            get { return m_primaryColor; }
+            set
+            {
+                m_primaryColor = value;               
+            }
+        }
+
+        public Color SecondaryBeamColor
+        {
+            get { return m_secondaryColor; }
+            set
+            {
+                m_secondaryColor = value;        
+            }
+        }
+        #endregion
+        Sandbox.ModAPI.IMyTerminalBlock terminalBlock;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            m_generator = Entity as Sandbox.ModAPI.IMyAssembler;
+            m_parent = Entity as IMyCubeBlock;
+            builder = objectBuilder;
+
+            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME | MyEntityUpdateEnum.EACH_100TH_FRAME;
+
+            terminalBlock = Entity as Sandbox.ModAPI.IMyTerminalBlock;
+        }
+        #region UpdateBeforeSimulation
+        public override void UpdateBeforeSimulation100()
+        {
+            base.UpdateBeforeSimulation100();
+            
+            if (m_generator.IsWorking)
+            {
+                if (IsInVoxel(m_generator as Sandbox.ModAPI.IMyTerminalBlock))
+                {
+                    IMyInventory inventory = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(0) as IMyInventory;
+                    VRage.MyFixedPoint amount = (VRage.MyFixedPoint)(140 * (1 + (0.6 * m_generator.UpgradeValues["Productivity"])));
+                    inventory.AddItems(amount, new MyObjectBuilder_Ore() { SubtypeName = "Stone" });
+                    terminalBlock.RefreshCustomInfo();
+                }
+            }
+        }
+        #endregion
+        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+        {
+            return builder;
+        }
+
+        private ulong m_counter = 0;
+
+        //draw counter
+        public override void UpdateBeforeSimulation()
+        {
+            m_counter++;
+            if (m_generator.IsWorking)
+            {
+                if (IsInVoxel(m_generator as Sandbox.ModAPI.IMyTerminalBlock))
+                {
+                    if (MyAPIGateway.Session?.Player == null)
+					{
+						return;
+					}
+					else
+					{
+						DrawBeams();
+					}				
+                }
+            }
+        }
+
      [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Assembler), true, "LargeBlockRig3")]
     public class Rig3 : MyGameLogicComponent
     {
@@ -218,7 +308,7 @@ namespace P3DResourceRig
                 if (IsInVoxel(m_generator as Sandbox.ModAPI.IMyTerminalBlock))
                 {
                     IMyInventory inventory = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(0) as IMyInventory;
-                    VRage.MyFixedPoint amount = (VRage.MyFixedPoint)(140 * (1 + (0.4 * m_generator.UpgradeValues["Productivity"])));
+                    VRage.MyFixedPoint amount = (VRage.MyFixedPoint)(300 * (1 + (0.8 * m_generator.UpgradeValues["Productivity"])));
                     inventory.AddItems(amount, new MyObjectBuilder_Ore() { SubtypeName = "Stone" });
                     terminalBlock.RefreshCustomInfo();
                 }
@@ -251,6 +341,9 @@ namespace P3DResourceRig
                 }
             }
         }
+
+
+
 
         private void DrawBeams()
         {
